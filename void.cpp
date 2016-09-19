5f1ffbedebf3cd5c51ef56d81a28987445b5f88d
ronaldroldanurrutia <ajedrez_11@hotmail.com>
2016-09-19 18:26:01 -0500

    Update void.cpp

--- void.cpp
+++ void.cpp
@@ -1,46 +1,43 @@
 #include<stdio.h>
 
 using namespace std;
-void mayuscula()
+char mayuscula(char a)
 {
-char a;
-scanf("%c",&a);
-a+=32;
-printf("%c",a);
-mayuscula();
+  a+=32;
+  return a;
 }
 float recibir(char *s)
 {
-float d=0,e;
-while(*s!='.')
-{
-e=(*s-'0');
-d=(d*10)+e;
-s++;
-}
-s++;
-float b,c=10,suma=0;
-while(*s!='e')
-{
-  b=(*s-'0');
-  suma+=(b/c);
-  c*=10;
+  float d=0,e;
+  while(*s!='.')
+  {
+    e=(*s-'0');
+    d=(d*10)+e;
+    s++;
+  }
   s++;
-}
-s+=2;
-d+=suma;
-int elemento=0;
-while(*s!='\0')
-{
-  elemento=(elemento*10)+(*s-'0');
-  s++;
-}
-for(int i=0;i<elemento;i++)
-{
-  d/=10;
-}
-printf("%f",d);
-}
+  float b,c=10,suma=0;
+  while(*s!='e')
+  {
+    b=(*s-'0');
+    suma+=(b/c);
+    c*=10;
+    s++;
+  }
+  s+=2;
+  d+=suma;
+  int elemento=0;
+  while(*s!='\0')
+  {
+    elemento=(elemento*10)+(*s-'0');
+    s++;
+  }
+  for(int i=0;i<elemento;i++)
+  {
+    d/=10;
+  }
+  printf("%f",d);
+  }
 void inversa(char *p,int numero,int inicio)
 {
     char q;
@@ -60,11 +57,11 @@ char *p;
 p=s;
 while(*p!='\0')
 {
-if(*p==b)
-{
-posicion=p-s;
-}
-p++;
+  if(*p==b)
+  {
+    posicion=p-s;
+  }
+  p++;
 }
 return posicion;
 }
@@ -73,5 +70,5 @@ main()
 recibir("10202.123e-2");
 invertida("Hola");
 alejado("Cienciadelacomputacion",'o');
-funcion();
+funcion('a');
 }
